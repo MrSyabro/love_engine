15a84932bb30e9760394a95e7964bb524a022901
MrSyabro <syabro1998@gmail.com>
2021-12-19 17:22:54 +0200

    Исправи функцию преобразования координат

--- utils.lua
+++ utils.lua
@@ -1,9 +1,12 @@
 local m = {}
+local w, h = love.window.getMode()
 
-function m.tc(ux, uy, w, h)
+function m.tc(ux, uy, _w, _h)
+    w, h = _w or w, _h or h
     return ux * w, uy * h
 end
-function m.tu(cx, cy, w, h)
+function m.tu(cx, cy, _w, _h)
+    w, h = _w or w, _h or h
     return cx / w, cy / h
 end
 
